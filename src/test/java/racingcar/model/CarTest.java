77341c6530f91a1279ee9f24876cee055a0a755b
0n1dev <all_mind@naver.com>
2022-10-09 19:01:34 +0900

    refactor: Car 테스트 케이스 수정
    - Car 테스트 케이스 수정

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -4,29 +4,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import racingcar.strategy.AlwaysMove;
 
 class CarTest {
 
     @Test
     void 자동차_이름_길이_예외_처리() {
         assertThatThrownBy(() -> {
-            new Car("test1234");
+            new Car("test1234", new AlwaysMove());
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 자동차_전진() {
-        Car car = new Car("test");
-        car.move(5);
+        Car car = new Car("test", new AlwaysMove());
+        car.move();
 
         assertThat(car.getDistance()).isEqualTo(1);
-    }
-
-    @Test
-    void 자동차_정지() {
-        Car car = new Car("test");
-        car.move(3);
-
-        assertThat(car.getDistance()).isEqualTo(0);
     }
 }
